049bc5be16bc45e3bfd820dcfb37aabb12b6c55f
joyboy241 <deepakwalker449@gmail.com>
2024-07-03 16:48:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "10758818"))
+API_HASH = getenv("API_HASH", "04fbbf6e526799e099494e33c891aa95")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGODB_CONNECTION_STRING", "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
+OWNER_ID = int(getenv("OWNER_ID", "728470953"))
+MONGODB_CONNECTION_STRING = getenv("MONGODB_CONNECTION_STRING", "mongodb+srv://joyboi242:deepak!123@atlascluster.58jejo7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1001692940921"))
 SESSION = getenv("SESSION", "")
-FORCESUB = getenv("FORCESUB", "")
+FORCESUB = getenv("FORCESUB", "-1001566404913")
